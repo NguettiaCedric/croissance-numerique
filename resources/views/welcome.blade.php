212bce1f84e93fb9814a62b4f36269d9019b842f
Aimé LINGUE <lingueaime@gmail.com>
2024-02-19 16:29:49 +0100

    Aime16h30

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
 
                     <!-- Slide 1 -->
                     <div class="carousel-item active"
-                        style="background-image: url('assets/img/slide/slide-1.png'); width:100%;">
+                        style="background-image: url('assets/img/slide/slide-5.png'); width:100%;">
                         <div class="carousel-container">
                             <div class="carousel-content container">
                                 <h2 class="animate__animated animate__fadeInDown">Welcome to <span>Mamba</span></h2>
